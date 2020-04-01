c10060f3d568a42369019eda077af121f1b1aabb
Daniil Bolshanin <redgreenmadfox@gmail.com>
2020-04-01 19:04:27 +0300

    first commit

--- script/script.js
+++ script/script.js
@@ -1,51 +1,35 @@
-let money = 60000,
-    income = 'фриланс',
-    addExpenses = 'интернет, такси, коммуналка',
-    deposit = true,
-    mission = 10000000,
-    period = 6;
+let lang = 'en';
+//if-else
 /*
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-    
-console.log(addExpenses.length);
-    
-console.log('период равен ' + period + ' месяцев');
-    
-console.log(addExpenses.toLocaleLowerCase().split(','));
-    
-let budgetDay = money / 30;
-console.log(budgetDay);
-*/
-money = prompt('Ваш ежемесячный доход?');
-console.log(money);
-
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log(addExpenses);
-
-deposit = confirm('Есть ли у вас депозит в банке?');
-console.log(deposit);
-
-let expenses1 = prompt('Введите обязательную статью расходов');
-let ammount1 = prompt('Во сколько это обойдется?');
-let expenses2 = prompt('Введите обязательную статью расходов');
-let ammount2 = prompt('Во сколько это обойдется?');
-
-let budgetMonth = money - ammount1 - ammount2;
-console.log(budgetMonth);
-
-let missionTime = Math.ceil(mission / budgetMonth);
-console.log(missionTime);
-
-let budgetDay = budgetMonth / 30;
-console.log(budgetDay);
-if(budgetDay >= 1200){
-    console.log('you have high level of income');
-} else if(budgetDay < 1200 && budgetDay >= 600){
-    console.log('you have medium level of income');
-} else if(budgetDay < 600){
-    console.log('you have low level of income');
-} else if(budgetDay < 0){
-    console.log('something went wrong');
+if(lang === 'ru'){
+    console.log('пн, вт, ср, чт, пт, сб, вск');
+} else if(lang === 'en'){
+    console.log('mon tue wed thurs fri sat sun');
 }
+*/
+//switch
+/*
+switch(lang){
+    case 'ru': 
+        console.log('пн, вт, ср, чт, пт, сб, вск');
+        break;
+    case 'en': 
+        console.log('mon tue wed thurs fri sat sun');
+        break;
+}
+*/
+//object.key
+/*
+let object = {
+    ru: 'пн, вт, ср, чт, пт, сб, вск',
+    en: 'mon tue wed thurs fri sat sun'
+};
+console.log(object.ru);
+console.log(object.en);
+*/
+let array = [
+    ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вск'],
+    ['mon', 'tue', 'wed', 'thurs', 'fri', 'sat', 'sun']
+];
+console.log(array[0]);
+console.log(array[1]);
